64567c049b01936e45d019539b292a4e3b0f8c01
Agrim Bansal <agrimbansal12@gmail.com>
2025-01-31 22:00:26 +0530

    Design changes almost almost done. Functionality left mostly

--- app/page.tsx
+++ app/page.tsx
@@ -2,26 +2,28 @@
 
 import Image from "next/image";
 import { useRef, useState } from "react";
-import { Toaster, toast } from "react-hot-toast";
-import DropDown, { VibeType } from "../components/DropDown";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LoadingDots from "../components/LoadingDots";
-import Toggle from "../components/Toggle";
-import { ChatCompletionStream } from "together-ai/lib/ChatCompletionStream";
 
 export default function Home() {
   const [loading, setLoading] = useState(false);
   const [bio, setBio] = useState("");
-  const [vibe, setVibe] = useState<VibeType>("Professional");
+  const [vibe, setVibe] = useState("Professional");
+
+  const [goal, setGoal] = useState("");
+  const [timeline, setTimeline] = useState("");
+  const [current, setCurrent] = useState("");
+  const [other, setOther] = useState("");
+
   const [generatedBios, setGeneratedBios] = useState<String>("");
   const [isLlama, setIsLlama] = useState(false);
 
-  const bioRef = useRef<null | HTMLDivElement>(null);
+  const outRef = useRef<null | HTMLDivElement>(null);
 
-  const scrollToBios = () => {
-    if (bioRef.current !== null) {
-      bioRef.current.scrollIntoView({ behavior: "smooth" });
+  const scrollToOutput = () => {
+    if (outRef.current !== null) {
+      outRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -33,9 +35,17 @@ export default function Home() {
     bio.slice(-1) === "." ? "" : "."
   }`;
 
-  const generateBio = async (e: any) => {
+  const runApp = async (e: any) => {
+    if (!goal || !timeline || !current) {
+      alert("Please fill out all required fields");
+      return;
+    }
+
+
     e.preventDefault();
+    
     setGeneratedBios("");
+
     setLoading(true);
     const response = await fetch("/api/together", {
       method: "POST",
@@ -54,89 +64,127 @@ export default function Home() {
       throw new Error(response.statusText);
     }
 
-    const runner = ChatCompletionStream.fromReadableStream(response.body!);
-    runner.on("content", (delta) => setGeneratedBios((prev) => prev + delta));
-
-    scrollToBios();
+    scrollToOutput();
     setLoading(false);
   };
 
   return (
     <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
-        <p className="border rounded-2xl py-1 px-4 text-slate-500 text-sm mb-5 hover:scale-105 transition duration-300 ease-in-out">
-          <b>126,657</b> bios generated so far
-        </p>
+
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12">
+        
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your next Twitter bio using AI
+          Set Goals. Achieve Them.
         </h1>
-        <div className="mt-7">
-          <Toggle isGPT={isLlama} setIsGPT={setIsLlama} />
+
+
+        <div className="w-full pt-8 space-y-4 grid grid-cols-2 gap-12">
+
+          <div className="border border-gray-200 p-4 rounded-xl">
+            <div className="flex items-center space-x-3">
+              <div className="flex items-center space-x-3">
+                <div className="h-8 w-8 rounded-3xl bg-black text-white flex items-center justify-center">1</div>
+                <p className="text-left font-medium">
+                  Set your Goal
+                </p>
+              </div>
+            </div>
+            <input
+              value={goal}
+              onChange={(e) => setGoal(e.target.value)}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+              placeholder={"Millionaire"}
+              />
+          </div>
+
+
+          <div className="border border-gray-200 p-4 rounded-xl">
+            <div className="flex items-center space-x-3">
+              <div className="flex items-center space-x-3">
+                <div className="h-8 w-8 rounded-3xl bg-black text-white flex items-center justify-center">2</div>
+                <p className="text-left font-medium">
+                  Timeline
+                </p>
+              </div>
+            </div>
+            <input
+              value={timeline}
+              onChange={(e) => setTimeline(e.target.value)}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+              placeholder={"5 Years"}
+              />
+          </div>
+
+          <div className="border border-gray-200 p-4 rounded-xl">
+            <div className="flex items-center space-x-3">
+              <div className="flex items-center space-x-3">
+                <div className="h-8 w-8 rounded-3xl bg-black text-white flex items-center justify-center">3</div>
+                <p className="text-left font-medium">
+                  Current Standing <span className="text-gray-600">(Describe yourself)</span>
+                </p>
+              </div>
+            </div>
+            <input
+              value={current}
+              onChange={(e) => setCurrent(e.target.value)}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+              placeholder={"Broke High Schooler "}
+              />
+          </div>
+
+          <div className="border border-gray-200 p-4 rounded-xl">
+            <div className="flex items-center space-x-3">
+              <div className="flex items-center space-x-3">
+                <div className="h-8 w-8 rounded-3xl bg-black text-white flex items-center justify-center">4</div>
+                <p className="text-left font-medium">
+                  Anything Else? <span className="text-gray-600">(Optional)</span>
+                </p>
+              </div>
+            </div>
+            <input
+              value={other}
+              onChange={(e) => setOther(e.target.value)}
+              className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+              placeholder={""}
+              />
         </div>
 
-        <div className="max-w-xl w-full">
-          <div className="flex mt-10 items-center space-x-3">
-            <Image
-              src="/1-black.png"
-              width={30}
-              height={30}
-              alt="1 icon"
-              className="mb-5 sm:mb-0"
-            />
-            <p className="text-left font-medium">
-              Drop in your job{" "}
-              <span className="text-slate-500">(or your favorite hobby)</span>.
-            </p>
-          </div>
-          <textarea
-            value={bio}
-            onChange={(e) => setBio(e.target.value)}
-            rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={"e.g. Amazon CEO"}
-          />
-          <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div>
+        </div>
+
+
           {loading ? (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-48"
               disabled
             >
               <LoadingDots color="white" style="large" />
             </button>
           ) : (
             <button
-              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
-              onClick={(e) => generateBio(e)}
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-48"
+              onClick={(e) => runApp(e)}
             >
-              Generate your bio &rarr;
+              Get Me There &rarr;
             </button>
           )}
-        </div>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-          toastOptions={{ duration: 2000 }}
-        />
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+
+
+
         <div className="space-y-10 my-10">
-          {generatedBios && (
+          {generatedBios || true && (
             <>
               <div>
                 <h2
                   className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
+                  ref={outRef}
                 >
-                  Your generated bios
+                  Here's How you get there
                 </h2>
               </div>
+
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                
                 {generatedBios
                   .substring(generatedBios.indexOf("1") + 3)
                   .split(/2\.|3\./)
@@ -144,23 +192,22 @@ export default function Home() {
                     return (
                       <div
                         className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
-                            icon: "✂️",
-                          });
-                        }}
                         key={generatedBio}
                       >
                         <p>{generatedBio}</p>
                       </div>
                     );
                   })}
+
               </div>
             </>
           )}
+
         </div>
+
+
       </main>
+
       <Footer />
     </div>
   );
